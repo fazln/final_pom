d5602a1c2b7d965961050859dd3ea95fb2f2c622
admin <test@list.com>
2025-05-11 14:35:47 +0500

    fixed error

--- pages/sale_page.py
+++ pages/sale_page.py
@@ -5,6 +5,9 @@ from pages.locators import sale_locators as sale_loc
 class SalePage(BasePage):
     page_url = '/sale.html'
 
-    def check_load_deals(self):
+    def check_deals_quantity_more_(self, quantity):
         deals = self.find_elements(sale_loc.DEALS)
-        assert len(deals) > 0
+        assert len(deals) > quantity, 'Not all deals are loaded'
+
+    def click_gear_link(self):
+        return self.click(sale_loc.GEAR)
